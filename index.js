e2ec9d43d9c59731700a9d36cf54ac38d0caf3d0
xlan11 <alankelly@gmail.com>
2022-07-05 22:29:17 +0100

    js updates

---
+++ index.js
@@ -0,0 +1,26 @@
+let homeScoreboard = document.getElementById("score-box-home")
+let homeCount = 0
+
+const homeScoreAddOne = document.getElementById("home-1");
+
+homeScoreAddOne.addEventListener("click", homePlusOne());
+
+function homePlusOne() {
+    homeCount = homeCount + 1
+    homeScoreboard.innerText = homeCount
+}
+
+function homePlusTwo() {
+    homeCount = homeCount + 2
+    homeScoreboard.innerText = homeCount
+}
+
+function homePlusThree() {
+    homeCount = homeCount + 3
+    homeScoreboard.innerText = homeCount
+}
+
+function resetScore() {
+    homeCount = 0;
+    homeScoreboard.innerText = homeCount;
+}
