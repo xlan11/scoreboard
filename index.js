356cdd6b46e1a99845b826c648da30753ece42f1
xlan11 <alankelly@gmail.com>
2022-07-06 22:25:52 +0100

    update with winning team

--- index.js
+++ index.js
@@ -10,21 +10,39 @@ const homeScoreCount1 = document.getElementById("home-1");
 const homeScoreCount2 = document.getElementById("home-2");
 const homeScoreCount3 = document.getElementById("home-3");
 
-homeScoreCount1.addEventListener("click", homePlusOne);
-homeScoreCount2.addEventListener("click", homePlusTwo);
-homeScoreCount3.addEventListener("click", homePlusThree);
+homeScoreCount1.addEventListener("click", homePlusOne, checkWinning());
+homeScoreCount2.addEventListener("click", homePlusTwo, checkWinning());
+homeScoreCount3.addEventListener("click", homePlusThree, checkWinning());
 
 const awayScoreCount1 = document.getElementById("away-1")
 const awayScoreCount2 = document.getElementById("away-2")
 const awayScoreCount3 = document.getElementById("away-3")
 
-awayScoreCount1.addEventListener("click", awayPlusOne);
-awayScoreCount2.addEventListener("click", awayPlusTwo);
-awayScoreCount3.addEventListener("click", awayPlusThree);
+awayScoreCount1.addEventListener("click", awayPlusOne, checkWinning());
+awayScoreCount2.addEventListener("click", awayPlusTwo, checkWinning());
+awayScoreCount3.addEventListener("click", awayPlusThree, checkWinning());
+
+let homeName = document.getElementById("team-name-home")
+let awayName = document.getElementById("team-name-away")
+
+function checkWinning() {
+    if (homeCount > awayCount) {
+        homeName.classList.add("winning")
+        awayName.classList.remove("winning")
+    }
+    else if (homeCount < awayCount) {
+        awayName.classList.add("winning")
+        homeName.classList.remove("winning")
+    }
+}
+
+checkWinning();
+console.log(checkWinning)
 
 function homePlusOne() {
     homeCount = homeCount + 1
     homeScoreboard.innerText = homeCount
+    checkWinning();
 }
 
 function homePlusTwo() {
@@ -55,4 +73,7 @@ function resetScore() {
     homeScoreboard.innerText = homeCount;
     awayCount = 0;
     awayScoreboard.innerText = awayCount;
+    homeName.classList.remove("winning")
+    awayName.classList.remove("winning")
 }
+
